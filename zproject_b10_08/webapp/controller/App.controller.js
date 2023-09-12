1ebe90b07313ad26bf375bbfcf9c5b410acaa2ff
mjhyun <mjh0328s@gmail.com>
2023-09-12 17:37:26 +0900

    main

---
+++ zproject_b10_08/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zprojectb1008.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ zproject_b10_08/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zproject_b10_08/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
